cb2084e1410254d6fe88432134111ddc00370fe1
Ana Sanchez <123514930+anasaunche@users.noreply.github.com>
2023-03-18 09:39:50 -0400

    Update and rename book_locator.py to book_locators.py

--- locators/book_locator.py
+++
@@ -1,5 +0,0 @@
-class BookLocators:
-    NAME = 'article.product_pod h3 a'
-    LINK= 'article.product_pod h3 a'
-    PRICE= 'article.product_pod p.price_color'
-    RATING= 'article.product_pod p.star-rating'
---
+++ locators/book_locators.py
@@ -0,0 +1,5 @@
+class BookLocators:
+    NAME = 'article.product_pod h3 a'
+    LINK = 'article.product_pod h3 a'
+    PRICE = 'article.product_pod div.product_price p.price_color'
+    RATING = 'article.product_pod p.star-rating'
