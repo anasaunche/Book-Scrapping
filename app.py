1baf8be324d39f90ec5ccfb851dafb9055205a64
Ana Sanchez <123514930+anasaunche@users.noreply.github.com>
2023-03-18 09:40:36 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,37 +1,11 @@
-logging.basicConfig(format = '%(asctime)s %levelname-8s [%(filename)s:%(lineno)d] %(message)s',
-                    datefmt = 'date: %d - %m - %Y time: %H:%M:%S',
-                    level = logging.INFO,
-                    filename = 'logs.txt')
+import requests
 
-logger = logging.getLogger('scraping')
+from page.all_books_page import AllBooksPage
 
-print('loading books list')
-logging.debug('loading book list')
+page_content = requests.get('http://books.toscrape.com').content
 
-logger.info('Requesting http://books.toscrape.com')
-page_content = requests.get('https://books.toscrape.com/').content
-
-logger.debug('Creating AllBooksPage from page content')
 page = AllBooksPage(page_content)
+books = page.books
 
-_books=[]
-
-start = time.time()
-logger.info(f'Going through {page.page_count} pages of books...')
-
-for page_num in (page.page_count):
-    page_start = time.time()
-    url = f'http://books.toscrape.com/catalogue/page-{page.page_count+1}.html'
-    logger.info(f'requesting {url}')
-    page_content = requests.get(url).content
-    logger.debug('Creating AllBooksPage from page content')
-    page = AllBooksPage(page_content)
-    print(f'{url} took {time.time() - page_start}')
-
-print(f'Total took {time.time() - start}')
-
-books = _books
-
-
-
-print(books)
+for book in books:
+    print(book)
